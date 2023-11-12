09125595ea7137359f8bf3ed0dc484470eb2c079
Huzefa <huzefazaheer1+git@gmail.com>
2023-11-12 19:48:03 +0500

    Added html to be able to display all books in library

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const myLibrary = [];
 
-const Book = function (author, name, noOfPages) {
+function Book(author, name, noOfPages) {
   this.author = author;
   this.name = name;
   this.noOfPages = noOfPages;
-};
+}
 
 Book.prototype.getInfo = function () {
   return `${this.name} is written by ${this.author} and has ${this.noOfPages} pages`;
@@ -13,3 +13,13 @@ Book.prototype.getInfo = function () {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+function displayLibrary() {
+  myLibrary.forEach((book) => {
+    const bookHTML = document.createElement("div");
+    bookHTML.innerHTML = `${book.name} </br> ${book.author} </br> ${book.noOfPages}`;
+    library.appendChild(bookHTML);
+  });
+}
+
+const library = document.querySelector(".library");
